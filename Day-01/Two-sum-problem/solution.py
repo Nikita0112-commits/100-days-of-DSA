ca99aa4bcf4eb61346df1e0c9fea5fee53059e99
Nikita0112-commits <nikita.das25@spit.ac.in>
2026-01-27 09:18:43 +0530

    Implement twoSum method in Solution class

---
+++ Day-01/Two-sum-problem/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_index = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in num_index:
+                return [num_index[complement], i]
+
+            num_index[num] = i
